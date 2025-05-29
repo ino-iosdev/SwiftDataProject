f5104c3d82a09141faeffa6c737d371d2a8319b5
ino yang popper <ino.yang.p@gmail.com>
2025-05-28 22:29:59 -0700

    added SwiftDataProject

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Ino Yang Popper on 5/28/25.
+//
+
+
+import SwiftData
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(for: User.self)
+    }
+}
